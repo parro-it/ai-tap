831bb7390eded74ec1e2c429b79e41c2e246fded
Andrea Parodi <andrea@parro.it>
2017-11-18 22:19:27 +0100

    test.async

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import test from "tape-async";
 import $package_var from ".";
 
-test("exports a function", t => {
+test("exports a function", async t => {
   t.is(typeof $package_var, "function");
 });
